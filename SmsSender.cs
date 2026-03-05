8958be1eaae47a358b0405f272d0fd270853c013
JoannaMercado <mercadojoanna983@gmail.com>
2026-03-05 22:46:07 +0800

    SmsSender.cs

--- SmsSender.cs
+++ SmsSender.cs
@@ -11,18 +11,18 @@ namespace Patrient_Record
 
         public async Task<bool> SendReminderSmsAsync(string toPhoneNumber, string message)
         {
-            var apiKey = "afd701ed"; // Replace with your API key
-            var apiSecret = "xDIO3nJuzJKIxoQs"; // Replace with your API secret
+            var apiKey = " "; // Replace with your API key
+            var apiSecret = " "; // Replace with your API secret
             var from = "639566481052"; // Replace with your sender ID or phone number
 
             var url = "https://rest.nexmo.com/sms/json";
             var content = new FormUrlEncodedContent(new[]
             {
-                new KeyValuePair<string, string>("afd701ed", apiKey),
-                new KeyValuePair<string, string>("xDIO3nJuzJKIxoQs", apiSecret),
+                new KeyValuePair<string, string>(" ", apiKey),
+                new KeyValuePair<string, string>(" ", apiSecret),
                 new KeyValuePair<string, string>("639566481052", from),
                 new KeyValuePair<string, string>("ContactNumber", toPhoneNumber),
-                new KeyValuePair<string, string>("HOY MAY GUMANA KA NA!!!", message)
+                new KeyValuePair<string, string>("Please Work", message)
             });
 
             try
@@ -44,3 +44,4 @@ namespace Patrient_Record
         }
     }
 }
+
